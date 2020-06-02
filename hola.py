92f6f5193500d92b3b8d10b1330c24cfe4897b3e
Cosmina Nicu <cosminanicu@gmail.com>
2020-06-02 18:54:38 +0200

    cambio

--- hola.py
+++ hola.py
@@ -4,3 +4,6 @@ books = pd.read_csv("Dataset/books.csv")
 ratings = pd.read_csv("Dataset/ratings.csv")
 tags = pd.read_csv("Dataset/tags.csv")
 book_tags = pd.read_csv("Dataset/book_tags.csv")
+
+
+#nuevo cambio
