f4674f66495703744507bd09844a01452526c0d9
Cosmina Nicu <cosminanicu@gmail.com>
2020-06-02 18:50:13 +0200

    test

---
+++ hola.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+books = pd.read_csv("Dataset/books.csv")
+ratings = pd.read_csv("Dataset/ratings.csv")
+tags = pd.read_csv("Dataset/tags.csv")
+book_tags = pd.read_csv("Dataset/book_tags.csv")
